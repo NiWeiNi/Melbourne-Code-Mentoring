a6e76a9e31f230e3957be688bcbdd5b80ce74065
Wei <weinini007@gmail.com>
2018-09-22 12:24:16 +1000

    Dependecies setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,36 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
 
 gulp.task('default', defaultTask);
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+});
+
+gulp.task('serve', ['sass'], function() {
+  browserSync.init({
+    server:"./"
+  });
+
+  gulp.watch("./scss/*.scss", ['sass']);
+  gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+gulp.task('sass', function() {
+  return gulp.src("./scss/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("app/css"))
+    .pipe(browserSync.stream());
+});
 
 function defaultTask(done) {
   // place code for your default task here
   done();
 }
+
+gulp.task('default', ['serve']);
